bcdcfe00565fa245c86c114bd6c367eaa5f5f6d4
Jul SQL <julsql1@gmail.com>
2025-09-29 15:39:07 +0200

    Initial Commit

---
+++ apps/apple/thecodeTests/thecodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  thecodeTests.swift
+//  thecodeTests
+//
+//  Created by Jul SQL on 29/09/2025.
+//
+
+import Testing
+@testable import thecode
+
+struct thecodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
